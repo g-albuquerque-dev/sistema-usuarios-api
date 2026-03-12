6bab4289726b2c640559a9bf99e2e0e6d2c4c6dd
Guilherme Albuquerque <125371746+GuilhermeAlbuquerqueC@users.noreply.github.com>
2026-03-12 17:06:05 -0300

    Configuração do banco H2, Criação da interface UsuarioRepository

---
+++ src/main/java/com/GuiDev/sistema_usuarios/usuario/Usuario.java
@@ -0,0 +1,27 @@
+package com.GuiDev.sistema_usuarios.usuario;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+}
